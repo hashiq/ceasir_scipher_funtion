f561866c909e8641d4d016da24b06843b0af9b1c
netsoft_pc <hashiqhussain028@gmail.com>
2026-05-14 18:01:14 +0400

    Commit

--- ceasir_scipher_funtion.py
+++ ceasir_scipher_funtion.py
@@ -4,10 +4,14 @@ alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 user_choice = input("type 'decode' for decode and type 'encode' for encode :")
 user_message = input("Enter the secret message:")
 shift_position = int(input("how much you want to shift:"))
+decodes=[]
 if user_choice == 'decode':
     def ceaser(message, shift):
         for i in message:
-            alphabets.index(i)
+            orginal_message = alphabets.index(i)
+            alphabets[orginal_message + shift_position]
 
-ceaser(message=user_message, shift=shift_position)
+
+decode_ = ceaser(message=user_message, shift=shift_position)
+print(decode_)
 
